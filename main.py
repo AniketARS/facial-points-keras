16b02a4eb6784b7694626f9fae823b7a353f0ad7
Aniket Bharti <aniket.code.ars@gmail.com>
2021-03-24 14:23:48 +0530

    tested data loading/processing pipeline

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import data_visualization
+from data_preprocessing import get_ds, process_ds
+import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-key_pts_frame = pd.read_csv('./data/training_frames_keypoints.csv')
+keys_frame_train = pd.read_csv('./data/training_frames_keypoints.csv')
+keys_frame_test = pd.read_csv('./data/test_frames_keypoints.csv')
 
-plt.figure(figsize=(10, 10))
-data_visualization.show_keypoints(idx=44, key_pts_frame=key_pts_frame)
-plt.show()
+train_ds = get_ds(keys_frame_train)
+test_ds = get_ds(keys_frame_test)
+
+train_ds = train_ds.map(process_ds(ds_type='training', new_shape=(224, 224, 3)), num_parallel_calls=tf.data.AUTOTUNE)
+test_ds = test_ds.map(process_ds(ds_type='testing', new_shape=(224, 224, 3)), num_parallel_calls=tf.data.AUTOTUNE)
+
+for i, k in iter(train_ds):
+    k = k.numpy() * 224.0
+    k = k.reshape(-1, 2)
+    plt.imshow(i)
+    plt.scatter(k[:, 0], k[:, 1], s=20, c='m', marker='.')
+    plt.show()
+    break
